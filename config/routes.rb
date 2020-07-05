cf6669f30f1547079a57ed7208a3f8384ea970dd
vagrant <vagrant@localhost.localdomain>
2020-07-05 07:13:58 +0000

    fixes#6

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+devise_for :users
+
+root :to => 'top#top'
+
+resources :users, only: [:show, :edit, :update, :index]
+resources :project, only: [:new, :create, :show, :edit, :update, :index, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
