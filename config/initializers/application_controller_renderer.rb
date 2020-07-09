3275cbe2b66a3a4e62efb67fe5d084f748c9469b
vagrant <vagrant@localhost.localdomain>
2020-07-09 02:38:52 +0000

    ユーザー情報編集機能実装

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9f383b4118da340226e95a4ac7ef64a8154c0f57d8bdaf827c579980e730b2ee8bb94b8319adcf5e756aac885eaf825805cd590822c2301bdf08b66f4c0a682e'
