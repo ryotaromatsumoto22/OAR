01d7e474fdc43e0f648e7c36ffdd62c9b3defb36
vagrant <vagrant@localhost.localdomain>
2020-07-10 02:41:18 +0000

    scaffold project

--- db/migrate/20200710022630_create_projects.rb
+++ db/migrate/20200710022630_create_projects.rb
@@ -1,13 +1,14 @@
 class CreateProjects < ActiveRecord::Migration[5.2]
   def change
     create_table :projects do |t|
-
-
       t.integer :user_id
       t.string :name
+      t.integer :hour
+      t.datetime :date
       t.string :goal
       t.string :period
-      t.integer :progress
+      t.integer :level
+
       t.timestamps
     end
   end
