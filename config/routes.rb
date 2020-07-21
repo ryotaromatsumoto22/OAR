695bb61fe2ace02ff02b7b5d7b5fa7798a00307b
vagrant <vagrant@localhost.localdomain>
2020-07-21 10:41:20 +0000

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ devise_for :users
 root :to => 'tops#top'
 
 resources :users, only: [:show, :edit, :update, :index]
-resources :projects, only: [:new, :create, :show, :edit, :update, :index, :destroy]
+resources :projects, only: [:new, :create, :show, :edit, :update, :index, :destroy] do
+	resource :favorites, only:[:create, :destroy]
+end
 resources :project_data, only: [:new, :create, :edit, :update, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
